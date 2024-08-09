ca9b64b4e35d888578676c6c3fa6fa6d579a2e8b
Billy-dev12 <lahjujuraja@gmail.com>
2024-08-09 11:17:44 +0700

    first commit

---
+++ lib/latihan2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class latihan2 extends StatelessWidget {
+  const latihan2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("anjay alok aselebewwwww"),
+      ),
+    );
+  }
+}
